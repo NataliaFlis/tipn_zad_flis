a714b314f4d9961f6ee80caaea7c5174d657700d
NataliaFlis <37382614+NataliaFlis@users.noreply.github.com>
2018-05-07 15:38:37 +0200

    Update and rename zadania 5 to Zadania5.R

---
+++ Zadania5.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(dplyr)
+
+
+#Zad 5-1
+library(readr)
+movies <- read_csv("D:/informatyka/Folder Git/drugi folder/tipn_zad_flis/movies.csv")
+View(movies)
+
+
+#Zad 5-2
+filter(movies, year == 2005, Comedy == 1)
+  
+
+#Zad 5-3
+movies %>% select(title, year, budget) %>% arrange(desc(budget))
+
+
+#Zad 5-4
+filter(movies, Animation == 1, year >=1990 & year < 2000) %>%
+  arrange(desc(rating))
+
+#Zad 5-5
+dramaty <- filter(movies, Drama == 1)
+arrange(dramaty, desc(length))
