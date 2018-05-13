8794df74b7d0ba5fd8d24ae4b7f341d6c54f7f31
NataliaFlis <37382614+NataliaFlis@users.noreply.github.com>
2018-05-13 17:26:48 +0200

    Update Zadania5.R

--- Zadania5.R
+++ Zadania5.R
@@ -23,3 +23,7 @@ filter(movies, Animation == 1, year >=1990 & year < 2000) %>%
 #Zad 5-5
 dramaty <- filter(movies, Drama == 1)
 arrange(dramaty, desc(length))
+
+#Zad 5-6
+mpaa_rating <- group_by(movies, mpaa) %>%
+  summarise (srednia = mean(rating), odchylenie = mad(rating))
